933ac8d3ba9c9782f49f95c72b24bc9fa7df9695
Lexx YungCarter <spencermainaa@gmail.com>
2020-09-19 13:24:36 +0300

    Swapped port to 8081

--- nodejs-express-mongodb/server.js
+++ nodejs-express-mongodb/server.js
@@ -82,7 +82,7 @@ require("./app/routes/user.routes")(app, router);
 require("./app/routes/role.routes")(app, router);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
