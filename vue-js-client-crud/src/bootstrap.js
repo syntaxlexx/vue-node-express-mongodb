eefbbc3c0f29ceeaa9c40c94ee0117f939f631ea
Lexx YungCarter <spencermainaa@gmail.com>
2020-09-19 13:51:22 +0300

    docs: updated readme

--- vue-js-client-crud/src/bootstrap.js
+++ vue-js-client-crud/src/bootstrap.js
@@ -8,7 +8,10 @@
 window._ = require('lodash');
 
 window.axios = require('axios');
-window.axios.defaults.baseURL = 'http://localhost:8080/api';
+window.axios.defaults.baseURL =
+    process.env.NODE_ENV == "production"
+        ? 'https://vuemongo.acelords.space/api'
+        : 'http://localhost:8081/api';
 
 // don't enable this else node POST requests will not pass body with payload
 // window.axios.defaults.headers.common['Content-type'] = 'application/json';
